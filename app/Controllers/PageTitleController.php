b6da1cbbe9fd7a64ebf09660e67f1f03abd1c6bf
Dmitriy Nemov <dmitrijnemov@mail.ru>
2024-04-12 16:44:06 +0300

    commit

--- app/Controllers/PageTitleController.php
+++ app/Controllers/PageTitleController.php
@@ -9,10 +9,10 @@ class PageTitleController
 public function index(){
     $smarty= new Smarty();
     $smarty->setTemplateDir('/../../smarty/templates/');
-    $smarty->setCompileDir('/../../smarty/templates_c');
-    $smarty->setCacheDir('/smarty/cache');
-    $smarty->setConfigDir('/smarty/configs');
+    $smarty->setCompileDir('/../../smarty/templates_c/');
+    $smarty->setCacheDir('/smarty/cache/');
+    $smarty->setConfigDir('/smarty/configs/');
     $smarty->assign('pageTitle','Home');
-    $smarty->display(template: 'template.tpl');
+    $smarty->display(template: 'template.tpl',cache_id: '',compile_id: 'compile.log');
 }
 }
