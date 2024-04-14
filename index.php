db4dfce1130c00dad0b81ec76c73004ad742ecb2
Dmitriy Nemov <dmitrijnemov@mail.ru>
2024-04-14 14:55:07 +0300

    routes3

--- index.php
+++ index.php
@@ -1,32 +1,15 @@
 <?php
-
-
-
 session_start();
-require_once __DIR__ . '/vendor/autoload.php';
-require_once __DIR__ . '/vendor/smarty/smarty/src/Smarty.php';
-$routes=require_once 'app/routes.php';
-$url=rtrim($_SERVER['REQUEST_URI'],'?');
-if(isset($routes[$url])){
-   [$controllerName, $action]=$routes[$url];
-}else{
-    echo 'Маршрут не найден';
-    $controllerName=false;
-    $action=false;
-}
-    if (!class_exists($controllerName)) {
-        echo 'Контроллер не существует';
-    }
-    $controller = new $controllerName();
 
-    if (!method_exists($controller, $action)) {
-        echo 'Метод не существует';
-    }
-    if (isset($url[1])) {
-        $controller->$action($url[1]);
-    } else {
-        $controller->$action();
-    }
-    exit;
+use App\App;
+
+define ('APP_PATH', __DIR__);
+
+require_once APP_PATH . '/vendor/autoload.php';
+
+$app=new App();
+
+$app->run();
+
 
 
